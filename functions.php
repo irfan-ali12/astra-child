306005cc110bf5853acf231e87817a6f46d3742f
Irfan Ali <irfanmarmaq@gmail.com>
2025-11-11 03:10:19 +0500

    basic setup

--- functions.php
+++ functions.php
@@ -23,3 +23,16 @@ function child_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+
+// Add theme support for child theme
+add_action( 'after_setup_theme', function() {
+    // Make sure parent loads first
+    add_theme_support( 'astra-child-theme' );
+});
+
+require get_stylesheet_directory() . '/inc/setup.php';
+require get_stylesheet_directory() . '/inc/enqueue.php';
+require get_stylesheet_directory() . '/inc/shortcodes.php';
+require get_stylesheet_directory() . '/inc/woocommerce-hooks.php';
+require get_stylesheet_directory() . '/inc/header-hooks.php';
+require get_stylesheet_directory() . '/inc/helpers.php';
